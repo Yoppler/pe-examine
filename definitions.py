d2aa2ca88c7428a17ecf390b72b34b65237166af
Yoppler <79474106+Yoppler@users.noreply.github.com>
2023-05-16 20:30:27 -0700

    Functional Project

---
+++ definitions.py
@@ -0,0 +1,204 @@
+from enum import Enum, IntEnum, Flag
+
+class Characteristics(Flag):
+    IMAGE_FILE_RELOCS_STRIPPED = 0x1
+    IMAGE_FILE_EXECUTABLE_IMAGE = 0x2
+    IMAGE_FILE_LINE_NUMS_STRIPPED = 0x4
+    IMAGE_FILE_LOCAL_SYMS_STRIPPED = 0x8
+    IMAGE_FILE_AGGRESSIVE_WS_TRIM = 0x10
+    IMAGE_FILE_LARGE_ADDRESS_AWARE = 0x20
+    IMAGE_FILE_BYTES_REVERSED_LO = 0x80
+    IMAGE_FILE_32BIT_MACHINE = 0x100
+    IMAGE_FILE_DEBUG_STRIPPED = 0x200
+    IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP = 0x400
+    IMAGE_FILE_NET_RUN_FROM_SWAP = 0x800
+    IMAGE_FILE_SYSTEM = 0x1000
+    IMAGE_FILE_DLL = 0x2000
+    IMAGE_FILE_UP_SYSTEM_ONLY = 0x4000
+    IMAGE_FILE_BYTES_REVERSED_HI = 0x8000
+    
+    def desc(self, flag):
+        return characteristics_descs[flag]
+
+characteristics_descs = {
+    "IMAGE_FILE_RELOCS_STRIPPED": "No base relocations",
+    "IMAGE_FILE_EXECUTABLE_IMAGE": "Image is executable",
+    "IMAGE_FILE_LINE_NUMS_STRIPPED": "COFF line numbers removed (deprecated)",
+    "IMAGE_FILE_LOCAL_SYMS_STRIPPED": "COFF symbol table entries removed (deprecated)",
+    "IMAGE_FILE_AGGRESSIVE_WS_TRIM": "Working set aggressively trimmed (obsolete)",
+    "IMAGE_FILE_LARGE_ADDRESS_AWARE": "Can handle > 2GB addresses",
+    "IMAGE_FILE_BYTES_REVERSED_LO": "Little endian (deprecated)",
+    "IMAGE_FILE_32BIT_MACHINE": "32-bit-word architecture",
+    "IMAGE_FILE_DEBUG_STRIPPED": "Debugging information removed",
+    "IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP": "If image is on removable media, load & copy to swap file",
+    "IMAGE_FILE_NET_RUN_FROM_SWAP": "If image is on network media, load & copy to swap file",
+    "IMAGE_FILE_SYSTEM": "System file, not user program",
+    "IMAGE_FILE_DLL": "Dynamic-link library (DLL) file",
+    "IMAGE_FILE_UP_SYSTEM_ONLY": "Run on uniprocessor only",
+    "IMAGE_FILE_BYTES_REVERSED_HI": "Big endian (deprecated)",
+}
+
+class DLLCharacteristics(Flag):
+    IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA = 0x20
+    IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE = 0x40
+    IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY = 0x80
+    IMAGE_DLLCHARACTERISTICS_NX_COMPAT = 0x100
+    IMAGE_DLLCHARACTERISTICS_NO_ISOLATION = 0x200
+    IMAGE_DLLCHARACTERISTICS_NO_SEH = 0x400
+    IMAGE_DLLCHARACTERISTICS_NO_BIND = 0x800
+    IMAGE_DLLCHARACTERISTICS_APPCONTAINER = 0x1000
+    IMAGE_DLLCHARACTERISTICS_WDM_DRIVER = 0x2000
+    IMAGE_DLLCHARACTERISTICS_GUARD_CF = 0x4000
+    IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE = 0x8000
+    
+    def desc(self, flag):
+        return dllcharacteristics_desc[flag]
+
+dllcharacteristics_desc = {
+    "IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA": "Can handle a high entropy 64-bit virtual address space",
+    "IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE": "DLL can be relocated at load time",
+    "IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY": "Code integrity checks are enforced",
+    "IMAGE_DLLCHARACTERISTICS_NX_COMPAT": "Image is Non-Execute (NX) compatible",
+    "IMAGE_DLLCHARACTERISTICS_NO_ISOLATION": "Isolation aware, but do not isolate the image",
+    "IMAGE_DLLCHARACTERISTICS_NO_SEH": "Does not use structured exception (SE) handling",
+    "IMAGE_DLLCHARACTERISTICS_NO_BIND": "Do not bind the image",
+    "IMAGE_DLLCHARACTERISTICS_APPCONTAINER": "Image must execute in an AppContainer",
+    "IMAGE_DLLCHARACTERISTICS_WDM_DRIVER": "A WDM driver",
+    "IMAGE_DLLCHARACTERISTICS_GUARD_CF": "Image supports Control Flow Guard",
+    "IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE": "Terminal Server aware",
+}
+
+class MachineType(IntEnum):
+    IMAGE_FILE_MACHINE_UNKNOWN = 0x0
+    IMAGE_FILE_MACHINE_ALPHA = 0x184
+    IMAGE_FILE_MACHINE_ALPHA64 = 0x284
+    IMAGE_FILE_MACHINE_AM33 = 0x1d3
+    IMAGE_FILE_MACHINE_AMD64 = 0x8664
+    IMAGE_FILE_MACHINE_ARM = 0x1c0
+    IMAGE_FILE_MACHINE_ARM64 = 0xaa64
+    IMAGE_FILE_MACHINE_ARMNT = 0x1c4
+    IMAGE_FILE_MACHINE_AXP64 = 0x284
+    IMAGE_FILE_MACHINE_EBC = 0xebc
+    IMAGE_FILE_MACHINE_I386 = 0x14c
+    IMAGE_FILE_MACHINE_IA64 = 0x200
+    IMAGE_FILE_MACHINE_LOONGARCH32 = 0x6232
+    IMAGE_FILE_MACHINE_LOONGARCH64 = 0x6264
+    IMAGE_FILE_MACHINE_M32R = 0x9041
+    IMAGE_FILE_MACHINE_MIPS16 = 0x266
+    IMAGE_FILE_MACHINE_MIPSFPU = 0x366
+    IMAGE_FILE_MACHINE_MIPSFPU16 = 0X466
+    IMAGE_FILE_MACHINE_POWERPC = 0x1f0
+    IMAGE_FILE_MACHINE_POWERPCFP = 0x1f1
+    IMAGE_FILE_MACHINE_R4000 = 0x166
+    IMAGE_FILE_MACHINE_RISCV32 = 0x5032
+    IMAGE_FILE_MACHINE_RISCV64 = 0x5064
+    IMAGE_FILE_MACHINE_RISCV128 = 0x5128
+    IMAGE_FILE_MACHINE_SH3 = 0x1a2
+    IMAGE_FILE_MACHINE_SH3DSP = 0x1a3
+    IMAGE_FILE_MACHINE_SH4 = 0x1a6
+    IMAGE_FILE_MACHINE_SH5 = 0x1a8
+    IMAGE_FILE_MACHINE_THUMB = 0x1c2
+    IMAGE_FILE_MACHINE_WCEMIPSV2 = 0x169
+    
+    def desc(self, value):
+        return machinetype_descs[value]
+
+machinetype_descs = {
+    "IMAGE_FILE_MACHINE_UNKNOWN": "Unknown",
+    "IMAGE_FILE_MACHINE_ALPHA": "Alpha AXP, 32-bit address space",
+    "IMAGE_FILE_MACHINE_ALPHA64": "Alpha 64, 64-bit address space",
+    "IMAGE_FILE_MACHINE_AM33": "Matsushita AM33",
+    "IMAGE_FILE_MACHINE_AMD64": "x64",
+    "IMAGE_FILE_MACHINE_ARM": "ARM little endian",
+    "IMAGE_FILE_MACHINE_ARM64": "ARM64 little endian",
+    "IMAGE_FILE_MACHINE_ARMNT": "ARM Thumb-2 little endian",
+    "IMAGE_FILE_MACHINE_AXP64": "AXP 64",
+    "IMAGE_FILE_MACHINE_EBC": "EFI byte code",
+    "IMAGE_FILE_MACHINE_I386": "Intel 386 or later processors and compatible processors",
+    "IMAGE_FILE_MACHINE_IA64": "Intel Itanium processor family",
+    "IMAGE_FILE_MACHINE_LOONGARCH32": "LoongArch 32-bit processor family",
+    "IMAGE_FILE_MACHINE_LOONGARCH64": "LoongArch 64-bit processor family", 
+    "IMAGE_FILE_MACHINE_M32R": "Mitsubishi M32R little endian",
+    "IMAGE_FILE_MACHINE_MIPS16": "MIPS16",
+    "IMAGE_FILE_MACHINE_MIPSFPU": "MIPS with FPU",
+    "IMAGE_FILE_MACHINE_MIPSFPU16": "MIPS16 with FPU",
+    "IMAGE_FILE_MACHINE_POWERPC": "Power PC little endian",
+    "IMAGE_FILE_MACHINE_POWERPCFP": "Power PC with floating point support",
+    "IMAGE_FILE_MACHINE_R4000": "MIPS little endian",
+    "IMAGE_FILE_MACHINE_RISCV32": "RISC-V 32-bit address space",
+    "IMAGE_FILE_MACHINE_RISCV64": "RISC-V 64-bit address space",
+    "IMAGE_FILE_MACHINE_RISCV128": "RISC-V 128-bit address space",
+    "IMAGE_FILE_MACHINE_SH3": "Hitachi SH3",
+    "IMAGE_FILE_MACHINE_SH3DSP": "Hitachi SH3 DSP",
+    "IMAGE_FILE_MACHINE_SH4": "Hitachi SH4",
+    "IMAGE_FILE_MACHINE_SH5": "Hitachi SH5",
+    "IMAGE_FILE_MACHINE_THUMB": "Thumb",
+    "IMAGE_FILE_MACHINE_WCEMIPSV2": "MIPS little-endian WCE v2",
+}
+
+class SectionFlags(Flag):
+    IMAGE_SCN_TYPE_NO_PAD = 0x8
+    IMAGE_SCN_CNT_CODE = 0x20
+    IMAGE_SCN_CNT_INITIALIZED_DATA = 0x40
+    IMAGE_SCN_CNT_UNINITIALIZED_DATA = 0x80
+    IMAGE_SCN_LNK_INFO = 0x200
+    IMAGE_SCN_LNK_REMOVE = 0x800
+    IMAGE_SCN_LNK_COMDAT = 0x1000
+    IMAGE_SCN_GPREL = 0x8000
+    IMAGE_SCN_ALIGN_1BYTES = 0x100000
+    IMAGE_SCN_ALIGN_2BYTES = 0x200000
+    IMAGE_SCN_ALIGN_4BYTES = 0x300000
+    IMAGE_SCN_ALIGN_8BYTES = 0x400000
+    IMAGE_SCN_ALIGN_16BYTES = 0x500000
+    IMAGE_SCN_ALIGN_32BYTES = 0x600000
+    IMAGE_SCN_ALIGN_64BYTES = 0x700000
+    IMAGE_SCN_ALIGN_128BYTES = 0x800000
+    IMAGE_SCN_ALIGN_256BYTES = 0x900000
+    IMAGE_SCN_ALIGN_512BYTES = 0xA00000
+    IMAGE_SCN_ALIGN_1024BYTES = 0xB00000
+    IMAGE_SCN_ALIGN_2048BYTES = 0xC00000
+    IMAGE_SCN_ALIGN_4096BYTES = 0xD00000
+    IMAGE_SCN_ALIGN_8192BYTES = 0xE00000
+    IMAGE_SCN_LNK_NRELOC_OVFL = 0x1000000
+    IMAGE_SCN_MEM_DISCARDABLE = 0x2000000
+    IMAGE_SCN_MEM_NOT_CACHED = 0x4000000
+    IMAGE_SCN_MEM_NOT_PAGED = 0x8000000
+    IMAGE_SCN_MEM_SHARED = 0x10000000
+    IMAGE_SCN_MEM_EXECUTE = 0x20000000
+    IMAGE_SCN_MEM_READ = 0x40000000
+    IMAGE_SCN_MEM_WRITE = 0x80000000
+    
+    def desc(self, flag):
+        return sectionflags_descs[flag]
+
+sectionflags_descs = {
+    "IMAGE_SCN_TYPE_NO_PAD": "The section should not be padded to the next boundary (obsolete)",
+    "IMAGE_SCN_CNT_CODE": "Contains executable code",
+    "IMAGE_SCN_CNT_INITIALIZED_DATA": "Contains initialized data",
+    "IMAGE_SCN_CNT_UNINITIALIZED_DATA": "Contains uninitialized data",
+    "IMAGE_SCN_LNK_INFO": "Contains comments or other information",
+    "IMAGE_SCN_LNK_REMOVE": "Will not become part of the image",
+    "IMAGE_SCN_LNK_COMDAT": "Contains COMDAT data",
+    "IMAGE_SCN_GPREL": "Contains data referenced through the global pointer (GP)",
+    "IMAGE_SCN_ALIGN_1BYTES": "Align data on a 1-byte boundary",
+    "IMAGE_SCN_ALIGN_2BYTES": "Align data on a 2-byte boundary",
+    "IMAGE_SCN_ALIGN_4BYTES": "Align data on a 4-byte boundary",
+    "IMAGE_SCN_ALIGN_8BYTES": "Align data on a 8-byte boundary",
+    "IMAGE_SCN_ALIGN_16BYTES": "Align data on a 16-byte boundary",
+    "IMAGE_SCN_ALIGN_32BYTES": "Align data on a 32-byte boundary",
+
+class WindowsSubsystem(IntEnum):
+    IMAGE_SUBSYSTEM_UNKNOWN = 0
+    IMAGE_SUBSYSTEM_NATIVE = 1
+    IMAGE_SUBSYSTEM_WINDOWS_GUI = 2
+    IMAGE_SUBSYSTEM_WINDOWS_CUI = 3
+    IMAGE_SUBSYSTEM_OS2_CUI = 5
+    IMAGE_SUBSYSTEM_POSIX_CUI = 7
+    IMAGE_SUBSYSTEM_NATIVE_WINDOWS = 8
+    IMAGE_SUBSYSTEM_WINDOWS_CE_GUI = 9
+    IMAGE_SUBSYSTEM_EFI_APPLICATION = 10
+    IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER = 11
+    IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER = 12
+    IMAGE_SUBSYSTEM_EFI_ROM = 13
+    IMAGE_SUBSYSTEM_XBOX = 14
+    IMAGE_SUBSYSTEM_WINDOWS_BOOT_APPLICATION = 16
---
+++ pe-examine.py
@@ -0,0 +1,347 @@
+import sys
+from pathlib import Path
+
+from dataclasses import dataclass
+from typing import Callable
+from enum import Enum
+import inspect
+from datetime import datetime
+from copy import deepcopy
+
+import definitions as defs
+
+@dataclass
+class Field:
+    name: str
+    offset: int
+    size: int
+    value: int | None = None
+    format: Callable[[int],str] | Enum | None = None
+    int: bool = True
+    
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, v):
+        if self.format is None:
+            self.fmt_value = v
+        elif inspect.isclass(self.format) and issubclass(self.format, Enum):
+            fmt = self.format(v).name
+            if "|" in fmt:
+                values = fmt.split("|")
+                fmt = fmt.replace("|", "\n" + " "*43)
+            else:
+               values = [fmt]
+            for v in values:
+                res = self.format.desc(v) + "\n" + " "*43
+            
+            self.fmt_value = res
+        elif callable(self.format):
+            self.fmt_value = self.format(v)
+        
+        self._value = v
+    
+    def __str__(self):
+        return f"{self.name:>40} : {self.fmt_value}"
+    
+    def __add__(self, other):
+        self.offset += other
+    
+    def __sub__(self, other):
+        self.offset -= other
+
+class Header:
+    def __init__(self, name, fields, start, content):
+        self.name = name
+        self.fields = fields
+        self.start = start
+        self.end = 0
+        self.content = content
+        self.size = 0
+    
+    @property
+    def fields(self):
+        return self._fields
+    
+    @fields.setter
+    def fields(self, value):
+        if not isinstance(value, list):
+            raise TypeError
+        
+        if not all([isinstance(item, Field) for item in value]):
+            raise TypeError
+        
+        self._fields = value
+    
+    @property
+    def content(self):
+        return self._content
+    
+    @content.setter
+    def content(self, value):
+        self._content = value
+        self.populate_fields()
+    
+    def populate_fields(self):
+        fields = [field + self.start for field in self.fields]
+        
+        for field in self.fields:
+            if field.int:
+                field.value = int.from_bytes(self.content[field.offset:field.offset + field.size], 'little')
+            else:
+                field.value = self.content[field.offset:field.offset + field.size]
+            
+            setattr(self, self.format_name(field.name), field.value)
+            
+            if field.offset + field.size > self.end:
+                self.end = field.offset + field.size
+    
+    def reverse_fields(self):
+        fields = [field - self.start for field in self.fields]
+    
+    def format_name(self, name):
+        name = name.lower().replace(" ", "_")
+        return name
+    
+    def __str__(self):
+        if self.name is None:
+            if hasattr(self, "header_name"):
+                name = self.header_name.replace(b'\x00', b'').decode('utf-8')
+            else:
+                raise ValueError
+        else:
+            name = self.name.upper()
+        res = "="*20 + f" {name} " + "="*20
+        res = f"{res:^80}\n"
+        
+        for field in self.fields:
+            res += str(field) + "\n"
+        
+        return res
+
+class Executable:
+    def __init__(self, filename):
+        self.filename = filename
+        self.DOS = Header("MS-DOS Stub", DOSHEADER, 0, self.content)
+        self.COFF = Header("COFF File Header", COFFHEADER, self.DOS.pe_header_address, self.content)
+        
+        # Not a great solution, but it works
+        Optional = Header("PE32 Header", PE32HEADER, self.COFF.end, self.content)
+        if Optional.magic_number == b"\x0b\x01":
+            self.Optional = Optional
+            self.Data_dirs = Header("PE32 Data Directories", PE32DATADIRECTORIES, self.COFF.end, self.content)
+        elif Optional.magic_number == b"\x0b\x02":
+            Optional.reverse_fields()
+            self.Optional = Header("PE32+ Header", PE32PLUSHEADER, self.COFF.end, self.content)
+            self.Data_dirs = Header("PE32+ Data Directories", PE32PLUSDATADIRECTORIES, self.COFF.end, self.content)
+                
+        section_table = []
+        start = self.COFF.end + self.COFF.optional_header_size
+        
+        # Deepcopy is a hacky kind of workaround
+        header_defs = deepcopy(SECTIONHEADERS)
+        for section in range(self.COFF.number_of_sections):
+            header = Header(None, header_defs, start, self.content)
+            section_table.append(header)
+            start = header.end
+            header_defs = deepcopy(SECTIONHEADERS)
+        
+        self.headers = [
+            self.DOS,
+            self.COFF,
+            self.Optional,
+            self.Data_dirs,
+        ]
+        
+        self.headers += section_table
+    
+    @property
+    def filename(self):
+        return self._filename
+    
+    @filename.setter
+    def filename(self, value):
+        path = Path(value)
+        
+        if not path.exists():
+            raise FileNotFoundError
+        
+        self._filename = path
+        
+        with open(value, "rb") as f:
+            self.content = f.read()
+    
+    def __str__(self):
+        res = "-"*20 + f" {self.filename.name.upper()} " + "-"*20
+        res = f"{res:^80}\n"
+        
+        for header in self.headers:
+            res += str(header) + "\n"
+        
+        return res
+
+def bytes_str(s):
+    if s >= 1024:
+        b = s / 1024
+        if b >= 1024:
+            c = b / 1024
+            return f"{c:.02f} MB"
+        return f"{b:.02f} KB"
+    return str(s) + " bytes"
+
+DOSHEADER = [
+    Field("Magic Number", 0, 2, int=False),
+    Field("PE Header Address", 0x3c, 4, format=hex), 
+]
+
+COFFHEADER = [
+    Field("Signature", 0, 4, int=False),
+    Field("Machine Type", 4, 2, format=defs.MachineType),
+    Field("Number of Sections", 6, 2),
+    Field("Time Date Stamp", 8, 4),
+    Field("Symbol Table Address", 12, 4, format=hex),
+    Field("Number of Symbols", 16, 4),
+    Field("Optional Header Size", 20, 2, format=bytes_str),
+    Field("Characteristics", 22, 2, format=defs.Characteristics)
+]
+
+COFFOPTIONALUNCHANGED = [
+    Field("Magic Number", 0, 2, int=False),
+    Field("Major Linker Version", 2, 1),
+    Field("Minor Linker Version", 3, 1),
+    Field("Size of Code", 4, 4, format=bytes_str),
+    Field("Size of Initialized Data", 8, 4, format=bytes_str),
+    Field("Size of Uninitialized Data", 12, 4, format=bytes_str),
+    Field("Entry Point Address", 16, 4, format=hex),
+    Field("Base of Code", 20, 4, format=hex),
+]
+
+COFFOPTIONALUNCHANGED2 = [
+    Field("Section Alignment", 32, 4, format=bytes_str),
+    Field("File Alignment", 36, 4, format=bytes_str),
+    Field("Major OS Version", 40, 2),
+    Field("Minor OS Version", 42, 2),
+    Field("Major Image Version", 44, 2),
+    Field("Minor Image Version", 46, 2),
+    Field("Major Subsystem Version", 48, 2),
+    Field("Minor Subsystem Version", 50, 2),
+    Field("Win32 Version Value", 52, 4),
+    Field("Size of Image", 56, 4, format=bytes_str),
+    Field("Size of Headers", 60, 4, format=bytes_str),
+    Field("Checksum", 64, 4, format=hex),
+    Field("Subsystem", 68, 2, format=defs.WindowsSubsystem),
+    Field("DLL Characteristics", 70, 2, format=defs.DLLCharacteristics),
+]
+
+PE32HEADER = [
+    *COFFOPTIONALUNCHANGED,
+    Field("Base of Data", 24, 4, format=hex),
+    Field("Image Base", 28, 4, format=hex),
+    *COFFOPTIONALUNCHANGED2,
+    Field("Size of Stack Reserve", 72, 4, format=bytes_str),
+    Field("Size of Stack Commit", 76, 4, format=bytes_str),
+    Field("Size of Heap Reserve", 80, 4, format=bytes_str),
+    Field("Size of Heap Commit", 84, 4, format=bytes_str),
+    Field("Loader Flags", 88, 4),
+    Field("Number of RVA and Sizes", 92, 4),
+]
+
+PE32DATADIRECTORIES = [
+    Field("Export Table Address", 96, 4, format=hex),
+    Field("Export Table Size", 100, 4, format=bytes_str),
+    Field("Import Table Address", 104, 4, format=hex),
+    Field("Import Table Size", 108, 4, format=bytes_str),
+    Field("Resource Table Address", 112, 4, format=hex),
+    Field("Resource Table Size", 116, 4, format=bytes_str),
+    Field("Exception Table Address", 120, 4, format=hex),
+    Field("Exception Table Size", 124, 4, format=bytes_str),
+    Field("Certificate Table Address", 128, 4, format=hex),
+    Field("Certificate Table Size", 132, 4, format=bytes_str),
+    Field("Base Relocation Table Address", 136, 4, format=hex),
+    Field("Base Relocation Table Size", 140, 4, format=bytes_str),
+    Field("Debug Data Address", 144, 4, format=hex),
+    Field("Debug Data Size", 148, 4, format=bytes_str),
+    Field("Architecture", 152, 8),
+    Field("Global Pointer", 160, 4, format=hex),
+    Field("TLS Table Address", 168, 4, format=hex),
+    Field("TLS Table Size", 172, 4, format=bytes_str),
+    Field("Load Config Table Address", 176, 4, format=hex),
+    Field("Load Config Table Size", 180, 4, format=bytes_str),
+    Field("Bound Import Table Address", 184, 4, format=hex),
+    Field("Bound Import Table Size", 188, 4, format=bytes_str),
+    Field("Import Address Table(IAT) Address", 192, 4, format=hex),
+    Field("Import Address Table(IAT) Size", 196, 4, format=bytes_str),
+    Field("Delay Import Descriptor Address", 200, 4, format=hex),
+    Field("Delay Import Descriptor Size", 204, 4, format=bytes_str),
+    Field("CLR Runtime Header Address", 208, 4, format=hex),
+    Field("CLR Runtime Header Size", 212, 4, format=bytes_str),
+]
+
+PE32PLUSHEADER = [
+    *COFFOPTIONALUNCHANGED,
+    Field("Image Base", 24, 8, format=hex),
+    *COFFOPTIONALUNCHANGED2,
+    Field("Size of Stack Reserve", 72, 8, format=bytes_str),
+    Field("Size of Stack Commit", 80, 8, format=bytes_str),
+    Field("Size of Heap Reserve", 88, 8, format=bytes_str),
+    Field("Size of Heap Commit", 96, 8, format=bytes_str),
+    Field("Loader Flags", 104, 4),
+    Field("Number of RVA and Sizes", 108, 4),
+]
+
+PE32PLUSDATADIRECTORIES = [
+    Field("Export Table Address", 112, 4, format=hex),
+    Field("Export Table Size", 116, 4, format=bytes_str),
+    Field("Import Table Address", 120, 4, format=hex),
+    Field("Import Table Size", 124, 4, format=bytes_str),
+    Field("Resource Table Address", 128, 4, format=hex),
+    Field("Resource Table Size", 132, 4, format=bytes_str),
+    Field("Exception Table Address", 136, 4, format=hex),
+    Field("Exception Table Size", 140, 4, format=bytes_str),
+    Field("Certificate Table Address", 144, 4, format=hex),
+    Field("Certificate Table Size", 148, 4, format=bytes_str),
+    Field("Base Relocation Table Address", 152, 4, format=hex),
+    Field("Base Relocation Table Size", 156, 4, format=bytes_str),
+    Field("Debug Data Address", 160, 4, format=hex),
+    Field("Debug Data Size", 164, 4, format=bytes_str),
+    Field("Architecture", 168, 8),
+    Field("Global Pointer", 176, 4, format=hex),
+    Field("TLS Table Address", 184, 4, format=hex),
+    Field("TLS Table Size", 188, 4, format=bytes_str),
+    Field("Load Config Table Address", 192, 4, format=hex),
+    Field("Load Config Table Size", 196, 4, format=bytes_str),
+    Field("Bound Import Table Address", 200, 4, format=hex),
+    Field("Bound Import Table Size", 204, 4, format=bytes_str),
+    Field("Import Address Table(IAT) Address", 208, 4, format=hex),
+    Field("Import Address Table(IAT) Size", 212, 4, format=bytes_str),
+    Field("Delay Import Descriptor Address", 216, 4, format=hex),
+    Field("Delay Import Descriptor Size", 220, 4, format=bytes_str),
+    Field("CLR Runtime Header Address", 224, 4, format=hex),
+    Field("CLR Runtime Header Size", 228, 4, format=bytes_str),
+]
+
+SECTIONHEADERS = [
+    Field("Header Name", 0, 8, int=False),
+    Field("Virtual Size", 8, 4, format=bytes_str),
+    Field("Virtual Address", 12, 4, format=hex),
+    Field("Size of Raw Data", 16, 4, format=bytes_str),
+    Field("Pointer to Raw Data", 20, 4, format=hex),
+    Field("Pointer to Relocations", 24, 4, format=hex),
+    Field("Pointer to Line Numbers", 28, 4, format=hex),
+    Field("Number of Relocations", 32, 2),
+    Field("Number of Linenumbers", 34, 2),
+    Field("Characteristics", 36, 4, format=defs.SectionFlags),
+]
+
+def main(fn):
+    exe = Executable(fn)
+    
+    print(exe)
+
+if __name__ == "__main__":
+    if not len(sys.argv) == 2:
+        print(f"Usage python {sys.argv[0]} <filename>")
+        exit()
+    main(sys.argv[1])
